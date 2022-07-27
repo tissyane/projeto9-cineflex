54e84346b1518367fc40f6cf0fbca98852f8e3cd
Tissyane <tissyane@gmail.com>
2022-07-26 23:21:21 -0300

    Get API movies list

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./Home.css";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const promise = axios.get(
+      "https://mock-api.driven.com.br/api/v5/cineflex/movies"
+    );
+
+    promise.then((response) => {
+      setMovies(response.data);
+    });
+  }, []);
+
   return (
-    <div className="title">
-      <h1>Selecione o filme</h1>
-    </div>
+    <>
+      <div className="title">
+        <h1>Selecione o filme</h1>
+      </div>
+      <div className="movies_container">
+        {movies.map((data) => (
+          <div className="poster">
+            <img key={data.id} src={data.posterURL} alt={data.title} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
